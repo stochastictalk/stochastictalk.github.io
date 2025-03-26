fe94998269f404d02bdd94296073b2b72d5b21f9
Jerome Wynne <56552295+stochastictalk@users.noreply.github.com>
2025-03-26 23:45:52 +0000

    Include circle.js.

---
+++ assets/js/○.js
@@ -0,0 +1,16 @@
+function setup() {
+    var canvas = createCanvas(400, 400, WEBGL);
+    canvas.parent("sketch")
+    angleMode(DEGREES);
+    strokeWeight(.2);
+    fill("white");
+    stroke(0, 0, 0);
+    smooth();
+  }
+  
+function draw() { 
+    background(255, 255, 255);
+    orbitControl();
+    sphere();
+    cursor(CROSS);
+  }
