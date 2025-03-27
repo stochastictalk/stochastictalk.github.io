3e1667a437242cdbfa5f7555bf3c6f76c0b1476f
Jerome Wynne <56552295+stochastictalk@users.noreply.github.com>
2025-03-27 19:42:51 +0000

    Include toggle color in dvd.js.

--- assets/js/dvd.js
+++ assets/js/dvd.js
@@ -1,20 +1,22 @@
+let c = 0;
 let c_w = 400;
 let c_h = 400;
-let img_h = 50;
-let img_w = 50;
+let img_h = 30;
+let img_w = Math.floor(2.21*img_h);
 let x = c_w/2;
 let y = c_h/2;
-let v = 1;
-let v_x = -1;
-let v_y = 0.5;
+let v_x = -2;
+let v_y = 2.1;
 let img;
 let min_x = img_w/2;
 let max_x = c_w - img_w/2;
 let min_y = img_h/2;
 let max_y = c_h - img_h/2;
+let threshold = 50;
 
 function preload() {
     img = loadImage('/assets/dvd.png');
+    img = toggleColor(img, c);
   }
 
 function setup() {
@@ -22,11 +24,31 @@ function setup() {
     canvas.parent("sketch");
 };
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+function toggleColor(img) {
+    color = [getRandomInt(255), getRandomInt(255), getRandomInt(255)]
+    img.loadPixels();
+
+    for (let i = 0; i < img.pixels.length; i += 4) {
+        if (img.pixels[i+3] > threshold) { // Non-transparent
+            img.pixels[i] = color[0];
+            img.pixels[i + 1] =  color[1];
+            img.pixels[i + 2] =  color[2];
+        }
+    }
+
+    img.updatePixels();
+
+    return img
+};
+
 
 function draw() {
-    background("white");
+    background("black");
     frameRate(60);
-    // The center bounds around a box of dim img_w - c_w
 
     x = x + v_x;
     y = y + v_y;
@@ -35,21 +57,29 @@ function draw() {
     if (x < min_x){
         x = x + 2*abs(min_x - x); // Update position
         v_x = -v_x;
+        c += 1;
+        img = toggleColor(img, c);
     }
 
     if (x > max_x){
         x = x - 2*abs(x - max_x);
         v_x = -v_x;
+        c += 1;
+        img = toggleColor(img, c);
     }
 
     if (y < min_y){
         y = y + 2*abs(min_y - y);
         v_y = -v_y;
+        c += 1;
+        img = toggleColor(img, c);
     }
 
     if (y > max_y){
         y = y - 2*abs(y - max_y);
         v_y = -v_y;
+        c += 1;
+        img = toggleColor(img, c);
     }
 
     image(img, x - img_w/2, y - img_h/2, img_w, img_h)
--- index.md
+++ index.md
@@ -9,6 +9,6 @@ permalink_name: readme.txt
 
  Hello! I'm Jerome. Welcome to my website. Take a look around. Enjoy yourself.
 
- I currently work at the [Artificial Intelligence Security Institute](aisi.gov.uk). If you would like to talk to me about a shared interest in connection with my work at AISI, you can email me at `jerome.wynne(at)dsit.gov.uk`.
+ I currently work at the [Artificial Intelligence Security Institute](https://aisi.gov.uk). If you would like to talk to me about a shared interest in connection with my work at AISI, you can email me at `jerome.wynne(at)dsit.gov.uk`.
 
 Alongside my work at AISI, I like to write and make things. You can read things I've written under [/posts](posts.md). If there is a project you are working on that you'd like to talk about, or you'd like to talk about something I've done or am doing, you can email me at `jerome.wynne(at)proton.me`.
