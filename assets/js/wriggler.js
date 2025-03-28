5522c324cd5792a34b94edbd3e47a3451592ab9f
Jerome Wynne <56552295+stochastictalk@users.noreply.github.com>
2025-03-28 18:22:26 +0000

    Include wriggler.js

--- assets/js/wriggler.js
+++ assets/js/wriggler.js
@@ -3,16 +3,18 @@ let c_h = 400;
 let mvmt;
 let pressedKeys = {};
 let OFFSET = 10000;
-let NOISE_SCALE = 0.01;
-let n_links = 10;
+let NOISE_SCALE = 0.005;
+let n_links = 50;
 let LINK_LENGTH = 1;
-let points =  Array(n_links).fill(0).map((e,i)=>createVector(50, 50));
+let points;
+let SPEED = 1;
 
 
 function setup() {
     let canvas = createCanvas(c_w, c_h);
     canvas.parent("sketch");
     mvmt = createVector(0, 0);
+    points = Array(n_links).fill(0).map((e,i)=>createVector(c_w/2, c_h/2));
 }
 
 function keyPressed() {
@@ -29,31 +31,33 @@ function keyReleased() {
 
 
 function draw() {
-    background("white");
-    stroke("black");
+    background("black");
+    stroke("white");
     strokeWeight(10);
 
-    if (pressedKeys.length > 0) {
-        mvmt = createVector(0,0);
 
-        if (pressedKeys.a) {
-            mvmt.x -= 1;
-        }
-        if (pressedKeys.w) {
-            mvmt.y += 1;
-        }
-        if (pressedKeys.s) {
-            mvmt.y -= 1;
-        }
-        if (pressedKeys.d) {
-            mvmt.x += 1;
-        }
-    } else {
-        mvmt.x = mvmt.x + (noise(NOISE_SCALE*points[0].x, NOISE_SCALE*points[0].y, NOISE_SCALE*frameCount)) / 2;
-        mvmt.y = mvmt.y + (noise(NOISE_SCALE*points[0].x + OFFSET, NOISE_SCALE*points[0].y + OFFSET, NOISE_SCALE*frameCount)) / 2;
+    mvmt = createVector(0,0);
+
+    if (pressedKeys.a) {
+        mvmt.x -= 1;
+    }
+    if (pressedKeys.w) {
+        mvmt.y -= 1;
+    }
+    if (pressedKeys.s) {
+        mvmt.y += 1;
+    }
+    if (pressedKeys.d) {
+        mvmt.x += 1;
+    }
+    
+    if (mvmt.mag() == 0.) {
+        mvmt.x = (mvmt.x + (noise(NOISE_SCALE*points[0].x, NOISE_SCALE*points[0].y, NOISE_SCALE*frameCount)) - 0.5) / 2;
+        mvmt.y = (mvmt.y + (noise(NOISE_SCALE*points[0].x + OFFSET, NOISE_SCALE*points[0].y + OFFSET, NOISE_SCALE*frameCount)) - 0.5) / 2;
     }
 
     // Update the head
+    mvmt.setMag(SPEED);
     points[0].add(mvmt);
 
     // Update the rest of the body
