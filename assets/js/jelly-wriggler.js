1edf01ea882ddb7afbaa1571dca6423334cdc0ec
Jerome Wynne <56552295+stochastictalk@users.noreply.github.com>
2025-03-30 20:09:25 +0000

    Add damping.

--- assets/js/jelly-wriggler.js
+++ assets/js/jelly-wriggler.js
@@ -3,36 +3,42 @@ let c_h = 400;
 let init_x = c_w/2;
 let init_y = c_w/2;
 let worm;
+let n_particles = 5;
+let k = 1;
+let m = 1;
+let dt = .1;
+let l = 10;
+let c = 1;
 
 class Worm {
-    constructor(n_particles, k, m, dt, l) {
+    constructor(n_particles, k, m, dt, l, c) {
         this.x = Array(n_particles).fill(0).map((e,i)=>createVector(c_w/2 - (n_particles/2 * 15) + (i * 15), c_h/2)); // Position of each particle
         this.v = Array(n_particles).fill(0).map((e,i)=>createVector(0., 0.)); // Velocity of each particle
-        this.k = k; // Link stiffness
+        this.k = k;
         this.n = n_particles;
-        this.m = m; // Mass of each particle
+        this.m = m;
         this.dt = dt;
         this.l = l;
+        this.c = c;
     }
 
     update() {
         for (let j = 0; j <  this.n; j++) {
-            let dv = createVector(0, 0);
-            if ((j > 0) && (j < this.n - 1)) {
-                dv.add(this.x[j+1]).sub(this.x[j]).add(this.x[j-1]).sub(this.x[j]);
+            let f1 = createVector(0, 0);
+            let f2 = createVector(0, 0);
+            if (j < this.n - 1) {
+                f1.add(this.x[j+1]).sub(this.x[j]);
+                f1.setMag(this.k*(f1.mag() - this.l));
+                f1.add(this.v[j+1].copy().sub(this.v[j]).mult(this.c));
             }
-            if (j == 0) { // Head
-                dv.add(this.x[j+1]).sub(this.x[j]); // TODO: need to subtract spring resting length
+            if (j > 0) {
+                f2.add(this.x[j-1]).sub(this.x[j]);
+                f2.setMag(this.k*(f2.mag() - this.l));
+                f2.add(this.v[j-1].copy().sub(this.v[j].mult(this.c)));
             }
-            if (j == this.n - 1) { // Tail
-                dv.add(this.x[j-1]).sub(this.x[j]);
-            }
-            dv.mult(this.k*this.dt/this.m); // dt x displacement x stiffness / mass
-
-            // Use these forces to update the particle velocity
-            this.v[j].add(dv);
-
-            // Use the new velocity to update the position
+            let f = p5.Vector.add(f1, f2);
+            let dv = p5.Vector.mult(f, this.dt/this.m);
+            this.v[j].add(dv).mult(0.99); // To stop numerical errors making the worm crazy
             let dx = this.v[j].copy();
             dx.mult(this.dt);
             this.x[j].add(dx);
@@ -43,9 +49,9 @@ class Worm {
 function setup() {
     canvas = createCanvas(c_w, c_h);
     canvas.parent("sketch");
-    strokeWeight(10);
+    strokeWeight(5);
     stroke("white");
-    worm = new Worm(10, 10, 1, .01, 10);
+    worm = new Worm(n_particles, k, m, dt, l, c);
 }
 
 function draw() {
@@ -56,6 +62,15 @@ function draw() {
 
     // Draw the worm
     for (let j = 0; j < worm.n; j++) {
-        point(worm.x[j].x, worm.x[j].y);
+
+        let x = worm.x[j].x % c_w;
+        let y = worm.x[j].y % c_h;
+        if (x < 0) {
+            x = c_w + x;
+        }
+        if (y < 0) {
+            y = c_h + y;
+        }
+        point(x, y);
     };
 }
