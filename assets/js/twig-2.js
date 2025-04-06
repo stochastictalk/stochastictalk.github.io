965308af31c46ad6843dbfe508a622ada537e19a
Jerome Wynne <56552295+stochastictalk@users.noreply.github.com>
2025-04-06 12:15:17 +0000

    Include replication of no-auxin configuration.

--- assets/js/twig-2.js
+++ assets/js/twig-2.js
@@ -1,5 +1,9 @@
 let c_w = 400;
 let c_h = 400;
+let canvas;
+let L = 1; // Scale
+let sw = 1; // Stroke weight
+let fr = 2; // Frame rate
 
 // Three cell types:
 // * Apexes
@@ -24,63 +28,91 @@ const CellType = Object.freeze({
     ANGLER: "angler"
 })
 
+const ALL_CELL_TYPES = [CellType.APEX, CellType.SLEEPER, CellType.ANGLER];
+
 class Tree {
 
-    constructor(x_o, y_o) {
-        this.x_o = x_o;
-        this.y_o = y_o;
-        this.stem = Cell() // Parent of all cells
+    constructor() {
+        this.stem_cell = new Cell(0., null); // Parent of all cells
+        this.stem_cell.x_o = 0.;
+        this.stem_cell.y_o = 0.;
+        this.stem_cell.x_e = this.stem_cell.x_o + Math.sin(this.stem_cell.th);
+        this.stem_cell.y_e = this.stem_cell.y_o + Math.cos(this.stem_cell.th);
     }
 
-    get apexes() { // TODO: returns list of apex cells
+    _extract_cells_of_type(cell, cell_collection, cell_types) { // Extract all descendent cells of a given type
+        // Parents are pushed before children
+        if (cell_types.includes(cell.type)) {
+            cell_collection.push(cell);
+        }
         
+        for (const child_cell of cell.c) {
+            this._extract_cells_of_type(child_cell, cell_collection, cell_types);
+        }
     }
 
-    get cells() { // TODO: returns list of all cells
-
+    // Returns all cells in tree of given types in a topologically sorted order
+    cells(cell_types) {
+        let cell_collection = [];
+        this._extract_cells_of_type(this.stem_cell, cell_collection, cell_types);
+        return cell_collection
     }
 
+    // Evolve the tree by one step
     update() {
+        // All the growth steps - replication, specialisation, budding, and angling - depend on auxin concentration.
+        // So we broadcast the auxin concentration from the stem tips and leaves, then run the individual grow steps.
+
         // Update auxin concentrations
-        for (const apex of this.apexes) {
-            apex.broadcast();
+        for (const cell of this.cells([CellType.APEX, CellType.SLEEPER])) {
+            cell.broadcast(); // Transmits its auxin concentration up to n steps in all directions
         }
 
         // Grow
-        for (const cell of this.cells) {
-            cell.replicate();
-            cell.specialise();
-            cell.bud();
-            cell.orient();
+        for (const cell of this.cells(ALL_CELL_TYPES)) {
+            this.age += 1;
+            cell.replicate(); // Branch apex cells replicate
+            cell.specialise(); // Apex cells specialise
+            cell.bud(); // Sleepers may bud to new branches
+            cell.orient(); // Anglers and sleeper orient themselves w.r.t. gravity
         }
     }
 
-    draw() { // TODO: compute position of all cells based on stem location and relative orientations
-
+    draw(o_x, o_y, L) {
+        // We draw the tree by figuring out the position of all the branches from the position of the stem cell
+        for (const cell of this.cells(ALL_CELL_TYPES)) {
+            if (!(cell.p == null)) {
+                cell.x_o = cell.p.x_e;
+                cell.y_o = cell.p.y_e;
+            }
+            cell.x_e = cell.x_o + Math.sin(cell.th);
+            cell.y_e = cell.y_o + Math.cos(cell.th);
+            line(o_x + L*cell.x_o, c_h - L*cell.y_o - o_y, o_x + L*cell.x_e, c_h - L*cell.y_e - o_y);
+        }
     }
-
 }
 
 class Cell {
 
-    constructor(th=0., p=[]) {
-        this.th = th; // orientation relative to parent
-        this.p = []; // list of parent cells
+    constructor(th=0., p) {
+        this.th = th; // global orientation relative to vertical, positive clockwise
+        this.p = p; // parent cell
         this.c = []; // list of children cells
         this.a = 0; // auxin concentration 
+        this.age = 0; // age
         this.type = CellType.APEX // _always_ an apex cell on construction
     }
 
     get replicate_probability() {
-        return 0.5; // TODO: should be a function of auxin concentration!
+        return 1.; // TODO: should be a function of auxin concentration!
     }
 
     get bud_probability() {
-        return 0.5; // TODO: should be a function of auxin concentration!
+        return 1.; // TODO: should be a function of auxin concentration!
     };
 
-    get sleeper_probability() {
-        return 0.5; // TODO: should be a function of auxin concentration!
+    get sleeper_probability() { // Probability of an apex specialising to a sleeper (i.e. a leaf)
+        return 0.05; // TODO: should be a function of auxin concentration!
     };
 
     get bud_angle() {
@@ -95,7 +127,8 @@ class Cell {
     // Apex methods
     replicate() { // if apex type, may extend existing branch
         if ((Math.random() <= this.replicate_probability) && (this.type == CellType.APEX)) {
-            this.children.push(new Cell(0., [this]));
+            let c = new Cell(this.th, this);
+            this.c.push(c);
         }
     }
 
@@ -109,11 +142,11 @@ class Cell {
 
     broadcast() { // if apex type or sleeper type, broadcasts auxin source value to neighbours
         if (this.type == CellType.APEX) {
-            
+            console.log("Apexes broadcast auxin.");
         }
         
         if (this.type == CellType.SLEEPER) {
-
+            console.log("Sleepers broadcast auxin.");
         }
     }
 
@@ -121,8 +154,8 @@ class Cell {
     bud() { // if sleeper type, may form a new branch and turn into an angler
         if ((Math.random() <= this.bud_probability) && (this.type == CellType.SLEEPER)) {
             this.type = CellType.ANGLER;
-            let c = new Cell(this.bud_angle, [this]);
-            this.children.push(c);
+            let c = new Cell(this.th + this.bud_angle, this);
+            this.c.push(c);
         }
     }
 
@@ -136,8 +169,15 @@ class Cell {
 function setup() {
     canvas = createCanvas(c_w, c_h);
     canvas.parent("sketch");
+    tree = new Tree();
+    frameRate(fr);
+    stroke("black");
+    strokeWeight(sw);
 };
 
 function draw() {
-    background("aqua");
+    background("white");
+    tree.update();
+    tree.draw(c_w/2, 0, L);
+    console.log(tree.cells(ALL_CELL_TYPES).length);
 }
