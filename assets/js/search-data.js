497617087a2d2345362520e61bb3cc591761ea4e
stochastictalk <56552295+stochastictalk@users.noreply.github.com>
2025-07-21 08:17:16 +0000

    Deploying to gh-pages from @ stochastictalk/stochastictalk.github.io@d95de9b9c22e9b51d797dd3ef511f822e53e30a4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,28 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-papers",
-          title: "papers",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/papers/";
-          },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "post-",
+  },{id: "post-",
         
           title: "",
         
